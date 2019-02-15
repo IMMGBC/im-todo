6cc497d5da16205f033eb9ac0538d31ce3a4ecb8
Bruno Bini <bruno.bonfim@georgebrown.ca>
2019-02-15 11:41:25 -0500

    Update main.js
    changed the id name
    included two ezxtra array

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 let todoRecord = [
   {
-    todoId: 123,
-    name: { subj: `WD`, inst: `Sgrignoli` },
+    todoId: Task1,
+    name: { subj: `MP`, inst: `Sgrignoli` },
     task: { total: 4, compl: `100%` }
   }, // 0
   {
-    todoId: 345,
+    todoId: Task2,
     name: { subj: `IP`, inst: `Masse` },
     task: { total: 7, compl: `50%` }
   }, // 1
   {
-    todoId: 567,
+    todoId: Task3,
     name: { subj: `PM`, inst: `Desgroseilliers` },
     task: { total: 9, compl: `10%` }
+  }, // 2
+  {
+    todoId: Task4,
+    name: { subj: `WD`, inst: `Pacinni` },
+    task: { total: 4, compl: `10%` }
+  }, // 2
+  {
+    todoId: Task5,
+    name: { subj: `PT`, inst: `Martin` },
+    task: { total: 1, compl: `10%` }
   } // 2
 ];
 
@@ -31,8 +41,8 @@ function showAllRecord() {
     todoRecord[0].task.total
   }% which is an ${todoRecord[0].task.compl}</li>`;
   allRecords.innerHTML += `<li>${todoRecord[1].name.subj} compl ${
-    todoRecord[1].grade.total
-  }% which is an ${studentRecord[1].grade.letter}</li>`;
+    todoRecord[1].name.total
+  }% which is an ${studentRecord[1].name.total}</li>`;
   allRecords.innerHTML += `<li>${todoRecord[2].name.subj} compl ${
     todoRecord[2].task.total
   }% which is an ${todotRecord[2].task.compl}</li>`;
@@ -40,7 +50,7 @@ function showAllRecord() {
 // 2. Add a refresh button to the interface to call this above function
 
 // Functions we will add:
-// addNewStudent()
-// deleteStudent()
-// showOneStudentRecord()
-// updateGrade()
+// addNewTask()
+// deleteTask()
+// showOneTaskRecord()
+// updateTask()
